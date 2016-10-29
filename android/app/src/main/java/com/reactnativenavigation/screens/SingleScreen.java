071e9fdcd899e48ff44d889d6c840afd4b4ec30a
Guy Carmeli <guyc@wix.com>
2016-10-29 11:47:58 +0300

    Move collapse logic from SingleScreen to CollapsingSingleScreen

--- android/app/src/main/java/com/reactnativenavigation/screens/SingleScreen.java
+++ android/app/src/main/java/com/reactnativenavigation/screens/SingleScreen.java
@@ -1,21 +1,15 @@
 package com.reactnativenavigation.screens;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ScrollView;
 
 import com.reactnativenavigation.params.ScreenParams;
 import com.reactnativenavigation.views.ContentView;
 import com.reactnativenavigation.views.LeftButtonOnClickListener;
-import com.reactnativenavigation.views.collapsingToolbar.CollapsingContentViewMeasurer;
-import com.reactnativenavigation.views.collapsingToolbar.CollapsingTopBar;
-import com.reactnativenavigation.views.collapsingToolbar.OnScrollViewAddedListener;
-import com.reactnativenavigation.views.collapsingToolbar.ScrollListener;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 public class SingleScreen extends Screen {
-    private ContentView contentView;
+    protected ContentView contentView;
 
     public SingleScreen(AppCompatActivity activity, ScreenParams screenParams,
                         LeftButtonOnClickListener titleBarBarBackButtonListener) {
@@ -23,54 +17,12 @@ public class SingleScreen extends Screen {
     }
 
     @Override
-    protected void createTopBar() {
-        if (screenParams.hasCollapsingTopBar()) {
-            final CollapsingTopBar topBar = new CollapsingTopBar(getContext(), styleParams.collapsingTopBarParams);
-            topBar.setScrollListener(new ScrollListener(topBar,
-                    new ScrollListener.OnScrollListener() {
-                        @Override
-                        public void onScroll(float amount) {
-                            contentView.collapse(amount);
-                            topBar.collapse(amount);
-                        }
-                    }
-            ));
-            this.topBar = topBar;
-        } else {
-            super.createTopBar();
-        }
-    }
-
-    @Override
     protected void createContent() {
         contentView = new ContentView(getContext(), screenParams.screenId, screenParams.navigationParams);
-        if (screenParams.hasCollapsingTopBar()) {
-            contentView.setViewMeasurer(new CollapsingContentViewMeasurer((CollapsingTopBar) topBar));
-            setupScrollDetection((CollapsingTopBar) topBar);
-        }
         addView(contentView, createLayoutParams());
     }
 
-    private void setupScrollDetection(final CollapsingTopBar topBar) {
-        contentView.setupScrollDetection(new ScrollListener(topBar,
-                new ScrollListener.OnScrollListener() {
-                    @Override
-                    public void onScroll(float amount) {
-                        contentView.collapse(amount);
-                        topBar.collapse(amount);
-                    }
-                }
-        ));
-        contentView.setOnScrollViewAddedListener(new OnScrollViewAddedListener() {
-            @Override
-            public void onScrollViewAdded(ScrollView scrollView) {
-                topBar.onScrollViewAdded(scrollView);
-            }
-        });
-    }
-
-    @NonNull
-    private LayoutParams createLayoutParams() {
+    protected LayoutParams createLayoutParams() {
         LayoutParams params = new LayoutParams(MATCH_PARENT, MATCH_PARENT);
         if (screenParams.styleParams.drawScreenBelowTopBar) {
             params.addRule(BELOW, topBar.getId());
