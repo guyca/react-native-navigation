bb4d339f1896a5d44d102d4549d3552a327749c8
Guy Carmeli <guyc@wix.com>
2018-01-07 16:50:48 +0200

    Changes required to run example app with our fork

--- example/scripts/postinstall.js
+++ example/scripts/postinstall.js
@@ -1,3 +1,5 @@
 const cp = require('child_process');
 cp.execSync(`rm -rf node_modules/react-native-navigation/node_modules`);
 cp.execSync(`rm -rf node_modules/react-native-navigation/example `);
+// HACK: Replace react-native dependency from maven with node_modules copy
+cp.execSync(`find . -type f -name build.gradle -not -path \"*/react-native/*\" -not -path \"*/Examples/*\" -not -path \"*/examples/*\" -print0 -exec sed -i '' \"s/.*com.facebook.react:react-native:.*/ compile project(':ReactAndroid')/\" {} +`);
